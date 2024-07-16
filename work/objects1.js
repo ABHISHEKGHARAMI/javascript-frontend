af804791409919203307758c34689e2c2c100123
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-16 23:49:22 +0530

    Adding the custom objects to the repo .

---
+++ work/objects1.js
@@ -0,0 +1,13 @@
+let objects = {
+    name : 'shocks',
+    price : 1059
+};
+
+console.log(objects);
+console.log(objects.name);
+console.log(objects.price);
+
+objects.new_property = false;
+objects.name = 'cotton shocks';
+
+console.log(objects);
