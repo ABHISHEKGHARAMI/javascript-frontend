b291a44ebc3e05e1e9ea7f59e87577bb06f98e18
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-17 00:26:27 +0530

    .mod

--- work/objects1.js
+++ work/objects1.js
@@ -11,3 +11,11 @@ objects.new_property = false;
 objects.name = 'cotton shocks';
 
 console.log(objects);
+
+// json stringify the object then log it
+let jsonObject = JSON.stringify(objects);
+console.log(jsonObject);
+
+// after that back to object
+let backObject = JSON.parse(jsonObject);
+console.log(backObject);
