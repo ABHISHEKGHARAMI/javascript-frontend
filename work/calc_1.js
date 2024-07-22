42f592486ca1f33a210ddd373320bc6c4d70b731
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-22 14:55:46 +0530

    .mod

--- work/calc_1.js
+++ work/calc_1.js
@@ -3,4 +3,24 @@ function myDateFunc(){
     let dob = document.getElementById("dob").value;
     console.log(name);
     console.log(dob);
+    
+    let age = calculateDate(new Date(dob));
+
+    let outputDiv = document.getElementById('output');
+    outputDiv.innerHTML="<p>Your submitted details are as follows : </p>"+
+                        "<p>hello there ! </p>"+ name +
+                        "<p>Your age is : </p>" + age;
+    
+}
+
+function calculateDate(dob){
+    let today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    let monthDiff = today.getMonth() - dob.getMonth();
+    let dayDiff = today.getDay() - dob.getDay();
+
+    if(monthDiff < 0 || monthDiff === 0 && dayDiff < 0){
+        age --;
+    }
+    return age;
 }
