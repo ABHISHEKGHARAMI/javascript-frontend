0284f2558b67cc792bfd375f4aa1fbd8c51e53ff
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-24 13:32:58 +0530

    Adding the button for the repo .

--- work/cal2.js
+++ work/cal2.js
@@ -9,3 +9,18 @@ function calFunc(){
         outputDiv.innerHTML = "The total cost of this cart is :  "+ chargeInt;
     }
 }
+
+
+// changing the subscribe and unsubscribe button
+function myFunc(){
+    let button = document.getElementById('subscribe-button');
+    if(button.textContent.toLowerCase()==='subscribe'){
+        button.textContent = "subscribed";
+        button.classList.remove('subscribe');
+        button.classList.add('subscribed');
+    }else{
+        button.textContent = "subscribe";
+        button.classList.remove('subscribed');
+        button.classList.add('subscribe');
+    }
+}
