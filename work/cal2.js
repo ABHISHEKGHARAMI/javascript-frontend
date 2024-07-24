8bf102cb9c460daceb46b2ae33a04b20e696c879
Abhishek Gharami <abhishekgharami1998@gmail.com>
2024-07-24 13:10:54 +0530

    .mod

--- work/cal2.js
+++ work/cal2.js
@@ -1,5 +1,11 @@
 function calFunc(){
     let charge = document.getElementById('charge').value;
     let chargeInt = parseInt(charge);
-    console.log(typeof chargeInt);
+    let outputDiv = document.getElementById('output');
+    if(chargeInt< 40){
+        chargeInt += chargeInt+ 10;
+        outputDiv.innerHTML = "The total cost of this cart is : "+ chargeInt;
+    }else{
+        outputDiv.innerHTML = "The total cost of this cart is :  "+ chargeInt;
+    }
 }
