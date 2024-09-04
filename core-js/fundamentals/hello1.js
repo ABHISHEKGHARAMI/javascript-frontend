1ef4bc4da958fde49f781cd31c6dcc92e51414a7
abhishek <abhishekgharami1998@gmail.com>
2024-09-04 15:50:06 +0530

    #Mod

--- core-js/fundamentals/hello1.js
+++ core-js/fundamentals/hello1.js
@@ -1,1 +1,17 @@
 console.log('hello world');
+
+// here goes the comment
+
+/*
+here goes
+multi line comments .
+*/
+
+// variable
+
+let var1 = 'this is a cow standing there';
+
+console.log(`the value of the var is : ${var1}`);
+
+console.log(`The type of the var is : ${typeof(var1)}`);
+
