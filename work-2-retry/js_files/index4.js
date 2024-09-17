8cf49615255f2812bad8e846c25dc6502e411e48
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 21:31:03 +0530

    # Mod

--- work-2-retry/js_files/index4.js
+++ work-2-retry/js_files/index4.js
@@ -3,5 +3,5 @@ function todoFunction(){
   let todo_element = document.querySelector('.js-name-input').value;
   todo_list.push(todo_element);
   console.log(todo_list);
-  
+  document.querySelector('.js-name-input').value = '';
 }
