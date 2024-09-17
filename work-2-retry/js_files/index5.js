7b955f4046d31826e9d8cc0d50f423f81168de6c
abhishek <abhishekgharami1998@gmail.com>
2024-09-17 22:18:46 +0530

    # Mod

---
+++ work-2-retry/js_files/index5.js
@@ -0,0 +1,16 @@
+let todo_list = [];
+
+function todoFunction(){
+    let todo_name = document.querySelector('.js-input').value;
+    let todo_date = document.querySelector('.js-time').value;
+    // appending the value as a object
+    todo_list.push({todo_name,todo_date});
+
+    // logging the list
+    let innerHTML = '';
+    for(let i =0 ;i < todo_list.length;i++){
+        let text = `<p>${todo_list[i]}</p>`
+        innerHTML += text;
+    }
+    document.querySelector('.js-inner').innerHTML = innerHTML;
+}
