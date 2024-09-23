30fb8a0b211ae910475a435ab1f5983b7780bc76
abhishek <abhishekgharami1998@gmail.com>
2024-09-23 11:35:00 +0530

    Adding the styles.

--- work-2-retry/js_files/index5.js
+++ work-2-retry/js_files/index5.js
@@ -1,23 +1,44 @@
 let todo_list = [];
 
-function addtofunction(){
+function todoFunction(){
     let todo_name = document.querySelector('.js-input').value;
-    let todo_date = document.querySelector('.js-date').value;
+    let todo_date = document.querySelector('.js-time').value;
+    // appending the value as a object
 
-    // add to the list
+    // checking the input is empty or not
+    if (todo_name == '' || todo_date == ''){
+        alert("Please enter the name or date!!!")
+    }
     todo_list.push({todo_name,todo_date});
+
+    // clearing the input
+    document.querySelector('.js-input').value = '';
+    document.querySelector('.js-time').value = '';
     render();
+
+
 }
 
+    // logging the list
+
 function render(){
-    let innerHtml = '';
-    for(let i = 0; i < todo_list.length;i++){
-      let text = `<p>Task name : ${todo_list[i]['todo_name']} time : ${todo_list[i]['todo_date']}
-      <button onclick="
-      todo_list.splice(${i},1);
-      ">Delete</button>
-      </p>`;
-      innerHtml += text;
+    let innerHTML = '';
+    for(let i =0 ;i < todo_list.length;i++){
+        let todo = todo_list[i];
+        let text = `
+        <div class="todo-item">
+           <span class="todo-name">${todo.todo_name} </span>
+           <span class="todo-date">Due date : ${new Date(todo.todo_date).toLocaleDateString()}</span>
+           <button class="delete-button" onclick="deleteTask(${i})">Delete</button>
+        </div>
+        `;
+        innerHTML += text;
     }
-    document.querySelector('.js-inner').innerHTML = innerHtml;
+    document.querySelector('.js-inner').innerHTML = innerHTML;
+}
+
+
+function deleteTask(index){
+    todo_list.splice(index,1);
+    render();
 }
